3b49c129feeca4cb085dd16daf7e60a66400696e
Tauseef Akbar Ali Shaikh <mohdtauseef.ma32@gmail.com>
2024-08-01 18:24:43 +0530

    Added components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 // src/components/Home.js
 import React from "react";
 import TypingEffect from "./TypingEffect";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
 import "./Home.css";
 
 const Home = () => {
@@ -17,6 +19,17 @@ const Home = () => {
           typeSpeed={100}
           backSpeed={50}
         />
+      </div>
+      <div className="resume-container">
+        <a
+          href="https://drive.google.com/file/d/1iJXuF8TWi8gypFAdQMAN0LvDHTD8xbuf/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="resume-link"
+        >
+          <FontAwesomeIcon icon={faFileAlt} className="resume-icon" />
+          <span>View My Resume</span>
+        </a>
       </div>
     </div>
   );
